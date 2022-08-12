cc5d20331bd1fbea71f3eb200b524cbeb592a10d
ncsfanbros2001 <ncssuperfan@gmail.com>
2022-08-12 17:36:51 +0700

    data

--- Restaurant/wwwroot/js/menuItem.js
+++ Restaurant/wwwroot/js/menuItem.js
@@ -5,9 +5,11 @@
             "type": "GET",
             "datatype": "json"
         },
-        "column": [
+        "columns": [
             { "data": "name", "width": "25%" },
-            { "data": "price", "width": "25%" },
+            { "data": "price", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
+            { "data": "foodType.name", "width": "15%" },
         ],
         "width": "100%"
     });
