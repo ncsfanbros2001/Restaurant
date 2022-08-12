857d242f1f97595605c36098c76e2c12af7a7d36
ncsfanbros2001 <ncssuperfan@gmail.com>
2022-08-12 10:41:18 +0700

    API

---
+++ Restaurant/Controllers/MenuItemController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant.Data.Repository;
+
+namespace Restaurant.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : Controller
+    {
+        private readonly IUnitOfWork _uow;
+
+        public MenuItemController(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var menuItemList = _uow.MenuItemRepository.GetAll();
+            return Json(new {data = menuItemList});
+        }
+    }
+}
--- Restaurant/Pages/Admin/MenuItemPages/Upsert.cshtml.cs
+++ Restaurant/Pages/Admin/MenuItemPages/Upsert.cshtml.cs
@@ -11,6 +11,7 @@ namespace Restaurant.Pages.Admin.MenuItemPages
     public class UpsertModel : PageModel
     {
         private readonly IUnitOfWork _uow;
+        private readonly IWebHostEnvironment _hostEnviroment;
 
         public MenuItem MenuItem { get; set; }
 
@@ -18,10 +19,11 @@ namespace Restaurant.Pages.Admin.MenuItemPages
 
         public IEnumerable<SelectListItem> FoodTypeList { get; set; }
 
-        public UpsertModel(IUnitOfWork uow)
+        public UpsertModel(IUnitOfWork uow, IWebHostEnvironment hostEnviroment)
         {
             _uow = uow;
             MenuItem = new();
+            _hostEnviroment = hostEnviroment;
         }
 
         public void OnGet()
@@ -39,14 +41,27 @@ namespace Restaurant.Pages.Admin.MenuItemPages
         }
 
         public IActionResult OnPost() {
-            //if (ModelState.IsValid)
-            //{
-            //    _uow.FoodTypeRepository.Add(MenuItem);
-            //    _uow.Save();
-            //    TempData["success"] = "Food Created Successfully";
-            //    return RedirectToPage("FoodTypeIndex");
-            //}
-            return Page();
+            string webRootPath = _hostEnviroment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+            if (MenuItem.Id == 0)
+            {
+                string newFileName = Guid.NewGuid().ToString();
+                var uploads = Path.Combine(webRootPath, @"images\menuItem");
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var fileStream = new FileStream(Path.Combine(uploads, newFileName + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(fileStream);
+                }
+                MenuItem.Image = @"images\menuItem\" + newFileName + extension;
+                _uow.MenuItemRepository.Add(MenuItem);
+                _uow.Save();
+            }
+            else
+            {
+
+            }
+            return RedirectToPage("MenuItemIndex");
         }
     }
 }
