790838962df865b129bf98e4f4428c691c92222a
ncsfanbros2001 <ncssuperfan@gmail.com>
2022-08-12 18:47:39 +0700

    buttons

--- Restaurant/wwwroot/js/menuItem.js
+++ Restaurant/wwwroot/js/menuItem.js
@@ -10,6 +10,20 @@
             { "data": "price", "width": "15%" },
             { "data": "category.name", "width": "15%" },
             { "data": "foodType.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="w-75 btn-group">
+                                <a href="/Admin/MenuItemPages/Upsert?id=${data} 
+                                    class="btn btn-success text-white mx-2"> 
+                                <i class="bi bi-pencil-square"></i> </a>
+                                <a href="/Admin/MenuItemPages/Upsert?id=${data} 
+                                    class="btn btn-danger text-white mx-2"> 
+                                <i class="bi bi-trash"></i> </a>
+                            </div>`
+                },
+                "width": "15%"
+            }
         ],
         "width": "100%"
     });
