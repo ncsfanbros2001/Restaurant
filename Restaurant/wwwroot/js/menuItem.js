734f78f29dbeded4cd5d4fc791522730353172f3
ncsfanbros2001 <ncssuperfan@gmail.com>
2022-08-12 22:02:15 +0700

    update

--- Restaurant/wwwroot/js/menuItem.js
+++ Restaurant/wwwroot/js/menuItem.js
@@ -14,10 +14,10 @@
                 "data": "id",
                 "render": function (data) {
                     return `<div class="w-75 btn-group">
-                                <a href="/Admin/MenuItemPages/Upsert?id=${data} 
+                                <a href="/Admin/MenuItemPages/Upsert?id=${data}" 
                                     class="btn btn-success text-white mx-2"> 
                                 <i class="bi bi-pencil-square"></i> </a>
-                                <a href="/Admin/MenuItemPages/Upsert?id=${data} 
+                                <a href="/Admin/MenuItemPages/Upsert?id=${data}"
                                     class="btn btn-danger text-white mx-2"> 
                                 <i class="bi bi-trash"></i> </a>
                             </div>`
